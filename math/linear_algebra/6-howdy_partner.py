28415520839db90014fa422585c324bfd9e88f48
fchavonet <fchavonet@mailo.com>
2024-06-03 20:31:58 +0200

    Uploading the first attempt for the task `6. Howdy Partners`.

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Module to concatenates two arrays.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays.
+
+    Parameters:
+        arr1 (list): the first array to concatenate.
+        arr2 (list): the second array to concatenate.
+
+    Returns:
+        list: a new array with the elements of arr1 followed by arr2.
+    """
+    return arr1 + arr2
