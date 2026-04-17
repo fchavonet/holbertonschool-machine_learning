25e8ef497061481b8e92a185dec3ee82d480789c
fchavonet <fchavonet@mail.fr>
2026-04-17 14:03:27 +0200

    Complete "Howdy Partner" task.

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Module that concatenates two lists
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two lists and returns a new list
+    """
+
+    return arr1 + arr2
