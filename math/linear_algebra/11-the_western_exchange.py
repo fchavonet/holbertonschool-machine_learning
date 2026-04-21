8657bcfa12ef463e332645c7cc85d0804913cce8
fchavonet <fchavonet@mail.fr>
2026-04-21 08:29:58 +0200

    Complete "The Western Exchange" task.

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Module that transposes a numpy array.
+"""
+
+import numpy as np
+
+
+def np_transpose(matrix):
+    """
+    Returns the transpose of a numpy.ndarray.
+    """
+
+    return np.transpose(matrix)
