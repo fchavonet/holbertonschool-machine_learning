640f4186270570d4568bc10d00639201c60c4284
fchavonet <fchavonet@mail.fr>
2026-04-21 08:28:33 +0200

    Complete "I’ll Use My Scale" task.

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Module that returns the shape of a numpy array.
+"""
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of a numpy.ndarray.
+    """
+
+    return matrix.shape
