9b5024d05f8d439f6e2efb4ed57c5eea42e8022d
fchavonet <fchavonet@mail.fr>
2026-04-21 08:34:36 +0200

    Complete "Saddle Up" task.

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module that performs matrix multiplication.
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Returns the matrix multiplication of two numpy arrays.
+    """
+
+    return np.matmul(mat1, mat2)
