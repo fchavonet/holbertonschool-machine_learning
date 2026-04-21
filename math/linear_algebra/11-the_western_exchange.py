decf887a12fc85cc8ee9a3dc500afb5ab804a8fe
fchavonet <fchavonet@mail.fr>
2026-04-21 08:31:14 +0200

    Fix: remove numpy import.

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -4,12 +4,10 @@
 Module that transposes a numpy array.
 """
 
-import numpy as np
-
 
 def np_transpose(matrix):
     """
     Returns the transpose of a numpy.ndarray.
     """
 
-    return np.transpose(matrix)
+    return matrix.T
